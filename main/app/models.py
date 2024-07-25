ae4e24b37eabb843431fffcc9c1ca0d3966daf21
oskar555-bit <arogundadeoluwatobi7@gmail.com>
2024-07-25 14:40:30 -0700

    Initial commit

---
+++ main/app/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    id= models.AutoField(primary_key=True)
+    title = models.CharField(max_length=55)
+    content = models.TextField()
+    image = models.ImageField(upload_to="Blog media")
+    created_at = models.DateTimeField(auto_now=True)
+    
+    
+    class meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+       return self.title
+   
+class Comment(models.Model):
+    id= models.AutoField(primary_key=True)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
+    
+    
+    class meta:
+        ordering = ['-created_at']
+        
+        def __str__(self):
+            return self.comment
+        
+class Like(models.Model):
+    id= models.AutoField(primary_key=True)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    like= models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now=True)
+    
+    class meta:
+        ordering = ['-created_at']
+
+class Newsletter(models.Model):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['-created_at']
+        
+        
+    def __str__(self):
+        return self.email
+        
+   
+        
